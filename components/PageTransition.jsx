893bb27bd59268a5b6d2bb5c48c7029057148d08
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-20 11:50:20 +0530

    setup page transition component and install framer motion

---
+++ components/PageTransition.jsx
@@ -0,0 +1,10 @@
+import { AnimatePresence } from 'framer-motion'
+import React from 'react'
+
+const PageTransition = ({children}) => {
+  return (
+    <AnimatePresence>PageTransition</AnimatePresence>
+  )
+}
+
+export default PageTransition
