eda99b1231a8db89c38c1591a942d6f216eeb8de
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-26 10:22:14 +0530

    Created hero section with download button

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Socials from "@/components/Socials";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -8,11 +9,29 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left">
-            <span>Software Developer</span>
-            <h1 className="h1">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
               Hello I'm <br />
-              <span>Nimas</span>
+              <span className="text-accent">Nimas</span>
             </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am
+              proficient in various programming languages and technologies.
+            </p>
+            {/* btn and socials*/}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials />
+              </div>
+            </div>
           </div>
           {/* photo */}
           <div>photo</div>
