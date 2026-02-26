f8db93c3b81fad91fc2a6399c849a4343e5f02f0
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-26 20:15:30 +0530

    Created Stats component

---
+++ components/Stats.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const states = [
+  {
+    num: 12,
+    text: "Years of experience",
+  },
+  {
+    num: 26,
+    text: "Projects completed",
+  },
+  {
+    num: 8,
+    text: "Technologies mastered",
+  },
+  {
+    num: 500,
+    text: "Code commits",
+  },
+];
+const Stats = () => {
+  return <div>Stats</div>;
+};
+
+export default Stats;
