3df4e478f348e534492da6ff1197bf9da601beb6
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-26 18:58:27 +0530

    Completed hero photo section

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div>Photo</div>
-  )
-}
+    <div className="w-full h-full relative">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+        >
+          <Image
+            src="/assets/photo2.jpeg"
+            alt="image"
+            priority
+            quality={100}
+            fill
+            className="object-contain rounded-full "
+          />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
 
-export default Photo
+export default Photo;
