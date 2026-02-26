2042ff30c3e209e70a22c026770d7121f0a6fe59
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-26 09:55:54 +0530

    Home page text

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-
-export default function Home() {
+const Home = () => {
   return (
-    <main>homepage</main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span>Software Developer</span>
+            <h1 className="h1">
+              Hello I'm <br />
+              <span>Nimas</span>
+            </h1>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
