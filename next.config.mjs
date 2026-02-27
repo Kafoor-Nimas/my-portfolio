8e013ef5bd84cdb8936e12daf041f4a77850daa3
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-27 20:01:02 +0530

    Refactor Services component for improved structure and add animation effects

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,17 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config) => {
+//     config.resolve.fallback = {
+//       ...config.resolve.fallback,
+//       process: false,
+//     };
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
