29797cbdefece86e6263fdef2bd3a6176aa06308
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-27 17:23:05 +0530

    Add services data structure to Services component

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,3 +1,36 @@
+"use client"
+
+import {BsArrowDownRight} from "react-icons/bs"
+import Link from "next/link";
+import { title } from "node:process";
+
+const services = [
+  {
+    num:"01",
+    title:'Web Development',
+    description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora earum.',
+    href:""
+  },
+  {
+    num:"02",
+    title:'UI/UX Design',
+    description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora earum.',
+    href:""
+  },
+  {
+    num:"03",
+    title:'Logo Design',
+    description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora earum.',
+    href:""
+  },
+  {
+    num:"04",
+    title:'SEO Optimization',
+    description:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora earum.',
+    href:""
+  },
+]
+
 const Services = () => {
   return <div>Services</div>;
 };
