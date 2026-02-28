1365c6f58bac1a4ce64203af031bc8d29d074ca8
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-28 15:06:07 +0530

    Work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,8 +1,26 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
 
 const Work = () => {
-  return (
-    <div>Work</div>
-  )
-}
+  return <div>Work</div>;
+};
 
-export default Work
+export default Work;
