8a070fe81cf49317047f70bd2024952862708b39
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-28 09:34:46 +0530

    Resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,19 @@
+"use client";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+  
+} from "react-icons/fa";
+
+import {
+  SiTailwindcss,SiNextdotjs,SiMongodb,SiExpress,SiGit,SiGithub,SiVercel
+} from "react-icons/si";
+
 const Resume = () => {
   return <div>Resume</div>;
 };
