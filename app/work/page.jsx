3e1f2b70eb48094f8507947a22d70f80cf8775dc
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-28 16:55:57 +0530

    add project details and update Work component layout

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -18,9 +18,65 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptate.",
+    stack: [
+      { name: "Html 5" },
+      { name: "Css 3" },
+      { name: "Javascript" },
+      { name: "React js" },
+    ],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    githubLink: "https://github.com/Kafoor-Nimas",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "project 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptate.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    githubLink: "https://github.com/Kafoor-Nimas",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptate.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    githubLink: "https://github.com/Kafoor-Nimas",
+  },
+];
 
 const Work = () => {
-  return <div>Work</div>;
+  const [project, setProject] = useState(projects[0]);
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full">text</div>
+          <div className="w-full">slider</div>
+        </div>
+      </div>
+      ;
+    </motion.section>
+  );
 };
 
 export default Work;
