884a982fc3f3636182cbad289e913c77601c09c7
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-01 17:56:52 +0530

    update home and social page

--- app/page.jsx
+++ app/page.jsx
@@ -11,14 +11,16 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Developer</span>
+            <span className="text-xl">MERN Full-Stack Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br />
+              Hello, I&apos;m <br />
               <span className="text-accent">Nimas</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I excel at crafting elegant digital experiences and I am
-              proficient in various programming languages and technologies.
+              I&apos;m a passionate Fullstack Developer specializing in the
+              MERN stack, currently expanding my expertise in Next.js and
+              TypeScript. I love building fast, scalable, and user-friendly
+              web applications from frontend to backend.
             </p>
             {/* btn and socials*/}
             <div className="flex flex-col xl:flex-row items-center gap-8">
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,19 +1,24 @@
 import Link from "next/link";
 
-import { FaGithub, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedin />, path: "" },
-  { icon: <FaYoutube />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/Kafoor-Nimas" },
+  { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/nimas-kafoor" },
+  { icon: <FaTwitter />, path: "https://twitter.com/FunNimas100" },
 ];
 
 const Socials = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
       {socials.map((item, index) => (
-        <Link key={index} href={item.path} className={iconStyles}>
+        <Link
+          key={index}
+          href={item.path}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={iconStyles}
+        >
           {item.icon}
         </Link>
       ))}
