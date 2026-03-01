25a3854b3227bc55ffa905380cb001fcbaeff155
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-01 13:47:10 +0530

    enhance Contact component layout with contact information and animation

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -14,8 +14,54 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+94) 778945679",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "nimaskafoor@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "129/5 Manikkampitiya, Kaduruwela, Polonnaruwa",
+  },
+];
+
+import { motion } from "framer-motion";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              form
+            </form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
