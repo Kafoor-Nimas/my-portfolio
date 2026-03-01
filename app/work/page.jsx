627d3ba6585e89b916257bf57e4ab9f129f3ea52
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-01 10:19:07 +0530

    add slider functionality to Work component with project image display

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -62,6 +62,13 @@ const projects = [
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
 
+  const handleSlideChange = (swiper) => {
+    // get current slide index
+    const currentIndex = swiper.activeIndex;
+    // update project state based on current slide index
+    setProject(projects[currentIndex]);
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -127,10 +134,36 @@ const Work = () => {
               </div>
             </div>
           </div>
-          <div className="w-full">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      {/* overlay */}
+                      <div></div>
+                      {/* image */}
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          alt=""
+                          className="object-cover"
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
         </div>
       </div>
-      
     </motion.section>
   );
 };
