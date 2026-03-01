531cee520f56009e4757a0ddb07dcff9451224a8
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-01 11:02:05 +0530

    enhance animation timing and layout of Work component for improved user experience

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -72,10 +72,14 @@ const Work = () => {
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
-      <div className="container mx-auto">
+      {/* <div className="container mx-auto"> */}
+      <div className="container mx-auto px-4 xl:px-16">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
@@ -154,6 +158,7 @@ const Work = () => {
                           fill
                           alt=""
                           className="object-cover"
+                          
                         />
                       </div>
                     </div>
