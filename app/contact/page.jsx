a5ab96f18746112549e48231887bd2f38ab0eb55
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-01 12:40:09 +0530

    Contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  return <div>Contact</div>;
+};
 
-export default Contact
+export default Contact;
