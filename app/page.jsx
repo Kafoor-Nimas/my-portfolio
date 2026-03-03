7933210e26eea3cca386ca93c13db01c3a207987
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-03 22:09:36 +0530

    added cv file and modify download cv buttod functionality

--- app/page.jsx
+++ app/page.jsx
@@ -24,14 +24,19 @@ const Home = () => {
             </p>
             {/* btn and socials*/}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
+              <a
+                href="/assets/cv/Nimas_Kafoor_CV.pdf"
+                download="Nimas_Kafoor_CV.pdf"
               >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
